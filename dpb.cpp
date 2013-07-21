8d611e879dd08455deefea629e9515d2cae14960
vehagn <veghag@gmail.com>
2013-07-21 19:17:55 +0200

    n

--- dpb.cpp
+++ dpb.cpp
@@ -13,7 +13,7 @@ int main( int argc, char *args[]){
     hd44780 lcd(14, 15, 24, 25, 8, 7);
     lcd.init(20, 4);
 
-    string a = "En streng!"
+    string a = "En streng!";
 
     lcd.write(0, 0, "0,1 Æ");
     lcd.write(1, 1, "1,1");
