421ed9139e7f2f6794a34f9d653242f1e6e4b1a9
vehagn <veghag@gmail.com>
2013-07-21 21:05:51 +0200

    Update dpb.cpp

--- dpb.cpp
+++ dpb.cpp
@@ -22,12 +22,14 @@ int main( int argc, char *args[]){
     uint8_t dtop[8]     = {0x00, 0x00, 0x00, 0x00, 0x04, 0x0e, 0x1f, 0x1b};
     uint8_t dleft[8]    = {0x03, 0x07, 0x00, 0x00, 0x1f, 0x1f, 0x00, 0x00};
     uint8_t dmid[8]     = {0x00, 0x00, 0x00, 0x00, 0x1f, 0x1f, 0x00, 0x00};
-    uint8_t dright[8]   = {0x18, 0x1c, 0x0e, 0x07, 0x1f, 0x1f, 0x00, 0x00};
+    uint8_t dright[8]   = {0x18, 0x1c, 0x0e, 0x07, 0x1f, 0x1f, 0x1b, 0x00};
     lcd.defChar(hd44780::CCHAR1, dtop);
     lcd.defChar(hd44780::CCHAR2, dleft);
     lcd.defChar(hd44780::CCHAR3, dmid);
     lcd.defChar(hd44780::CCHAR4, dright);
+    
 
+    
 
 
     lcd.write("0,1 Æ\n",200);
@@ -43,8 +45,58 @@ int main( int argc, char *args[]){
     lcd.write(10,1, hd44780::CCHAR1);
     lcd.write(9,2, hd44780::CCHAR2);
     lcd.write(10,2, hd44780::CCHAR3);
-    lcd.write(11,2, hd44780::CCHAR4);
+    lcd.write(11,2, hd44780::CCHAR4,500);
+    
+    lcd.clear();
 
+    uint8_t delta_TL[8]     = {0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x03, 0x07};
+    lcd.defChar(hd44780::CCHAR0, delta_TL);
+    lcd.write(3,0,hd44780::CCHAR0);
+    
+    uint8_t delta_TM[8]     = {0x00, 0x04, 0x0e, 0x1f, 0x1f, 0x1f, 0x1f, 0x11};
+    lcd.defChar(hd44780::CCHAR0, delta_TM);
+    lcd.write(4,0,hd44780::CCHAR0);
+    
+    uint8_t delta_TR[8]     = {0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x18, 0x1c};
+    lcd.defChar(hd44780::CCHAR0, delta_TR);
+    lcd.write(5,0,hd44780::CCHAR0);
+    
+    uint8_t delta_MLL[8]    = {0x00, 0x00, 0x01, 0x03, 0x07, 0x0f, 0x1f, 0x00};
+    lcd.defChar(hd44780::CCHAR0, delta_MLL);
+    lcd.write(2,1,hd44780::CCHAR0);
+    
+    uint8_t delta_MLR[8]    = {0x1f, 0x1e, 0x1c, 0x18, 0x10, 0x00, 0x00, 0x00};
+    lcd.defChar(hd44780::CCHAR0, delta_MLR);
+    lcd.write(3,1,hd44780::CCHAR0);
+    
+    uint8_t delta_MRL[8]    = {0x1f, 0x0f, 0x07, 0x03, 0x01, 0x00, 0x00, 0x00};
+    lcd.defChar(hd44780::CCHAR0, delta_MRL);
+    lcd.write(5,1,hd44780::CCHAR0);
+    
+    uint8_t delta_MRR[8]    = {0x00, 0x00, 0x10, 0x18, 0x1c, 0x1e, 0x1f, 0x0f};
+    lcd.defChar(hd44780::CCHAR0, delta_MRR);
+    lcd.write(6,1,hd44780::CCHAR0);
+    
+    uint8_t delta_BL[8]     = {0x00, 0x00, 0x0f, 0x1f, 0x1f, 0x0f, 0x00, 0x00};
+    lcd.defChar(hd44780::CCHAR0, delta_BL);
+    lcd.write(1,2,hd44780::CCHAR0);
+    
+    uint8_t delta_BM[8]     = {0x00, 0x00, 0x1f, 0x1f, 0x1f, 0x1f, 0x00, 0x00};
+    lcd.defChar(hd44780::CCHAR0, delta_BM);
+    lcd.write(2,2,hd44780::CCHAR0);
+    lcd.write(3,2,hd44780::CCHAR0);
+    lcd.write(4,2,hd44780::CCHAR0);
+    lcd.write(5,2,hd44780::CCHAR0);
+    
+    uint8_t delta_BRL[8]    = {0x03, 0x01, 0x1f, 0x1f, 0x1f, 0x1f, 0x00, 0x00};
+    lcd.defChar(hd44780::CCHAR0, delta_BRL);
+    lcd.write(6,2,hd44780::CCHAR0);
+    
+    uint8_t delta_BRR[8]    = {0x18, 0x1c, 0x1e, 0x1f, 0x1f, 0x1e, 0x00, 0x00};
+    lcd.defChar(hd44780::CCHAR0, delta_BRL);
+    lcd.write(7,2,hd44780::CCHAR0);
+    
+    lcd.write(2,3,"-DELTA-",500)
 
 
     return 0;
