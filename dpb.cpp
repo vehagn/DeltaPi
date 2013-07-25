179b7d51444e8bf0bcf600e023fa528b0cdafc1b
vehagn <veghag@gmail.com>
2013-07-25 23:32:14 +0200

    Update dpb.cpp

--- dpb.cpp
+++ dpb.cpp
@@ -6,6 +6,28 @@
 #include <sstream>
 #include <string>
 #include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
+char getch() {
+        char buf = 0;
+        struct termios old = {0};
+        if (tcgetattr(0, &old) < 0)
+                perror("tcsetattr()");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &old) < 0)
+                perror("tcsetattr ICANON");
+        if (read(0, &buf, 1) < 0)
+                perror ("read()");
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+        if (tcsetattr(0, TCSADRAIN, &old) < 0)
+                perror ("tcsetattr ~ICANON");
+        return (buf);
+}
 
 
 using namespace rpihw;
@@ -68,11 +90,12 @@ int main( int argc, char *args[]){
     }*/
     lcd.clear();
     
-    char c;
+    char c[2];
+    c[1] = '\n';
     do {
-        c = getchar();
+        c[0] = getch();
         lcd.write(c);
-    }while (c != '\n');
+    }while (c[0] != '\n');
 
     return 0;
 }
