453af9db8c3f44e2c1fac3d68db220a3a4a69dfe
vehagn <veghag@gmail.com>
2013-07-25 23:54:29 +0200

    Update dpb.cpp

--- dpb.cpp
+++ dpb.cpp
@@ -9,25 +9,7 @@
 #include <unistd.h>
 #include <termios.h>
 
-char getch() {
-        
-        /*
-        struct termios old = {0};
-        if (tcgetattr(0, &old) < 0)
-                perror("tcsetattr()");
-        old.c_lflag &= ~ICANON;
-        old.c_lflag &= ~ECHO;
-        old.c_cc[VMIN] = 1;
-        old.c_cc[VTIME] = 0;
-        if (tcsetattr(0, TCSANOW, &old) < 0)
-                perror("tcsetattr ICANON");
-        if (read(0, &buf, 1) < 0)
-                perror ("read()");
-        old.c_lflag |= ICANON;
-        old.c_lflag |= ECHO;
-        if (tcsetattr(0, TCSADRAIN, &old) < 0)
-                perror ("tcsetattr ~ICANON");
-        return (buf);*/     
+char getch() {  
         struct termios old_tio, new_tio;
         unsigned char buf = 0;
         
@@ -104,12 +86,15 @@ int main( int argc, char *args[]){
     }*/
     lcd.clear();
     
-    unsigned char c;
+    unsigned char c, buf[8];
     int i = 0;
     do {
         c = getch();
+        buf[i] = c;
         lcd.write(i++%20,0,c);
     }while (c != '\n');
+    
+    printf("\n %i \n",atoi(buf));
 
     return 0;
 }
