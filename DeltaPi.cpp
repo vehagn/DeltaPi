d1815561bc652ecf9f28be440c1ceafaeee59033
vehagn <veghag@gmail.com>
2013-07-28 00:48:09 +0200

    Update DeltaPi.cpp

--- DeltaPi.cpp
+++ DeltaPi.cpp
@@ -4,10 +4,17 @@ map<const int, Entry> entries;
 Entry person;
 
 int main(int argc, char* argv[]){
+
+	hd44780 lcd(14, 15, 24, 25, 8, 7);
+   	lcd.init(20, 4);
+	
 	retrieveSQL(entries);
 	entries.erase(0);
 	
-	cout << entries.find(10993472)->second.getNameFirstLast();
+	char strbuf[256];
+	
+	sprintf(strbuf, "%s", entries.find(10993472)->second.getNameFirstLast());
+	printfl(strbuf, &lcd);
 
 
 	return 0;
