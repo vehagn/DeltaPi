7a2a3486866c309c86c0c3dfbd5744b5636456d6
vehagn <veghag@gmail.com>
2013-07-27 23:48:00 +0200

    Update DeltaPi.cpp

--- DeltaPi.cpp
+++ DeltaPi.cpp
@@ -7,7 +7,7 @@ int main(int argc, char* argv[]){
 	retrieveSQL(entries);
 	entries.erase(0);
 	
-	printf("\n %s \n", entries.find(10993472)->second.getNameFirstLast());
+	cout << entries.find(10993472)->second.getNameFirstLast();
 
 
 	return 0;
