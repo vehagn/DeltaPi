dc8ac048daf7203953c95fe483734f3cb3654b9a
vehagn <veghag@gmail.com>
2013-07-28 03:07:59 +0200

    Update DeltaPi.cpp

--- DeltaPi.cpp
+++ DeltaPi.cpp
@@ -7,6 +7,7 @@ int main(int argc, char* argv[]){
 
 	hd44780 lcd(14, 15, 24, 25, 8, 7);
    	lcd.init(20, 4);
+   	lcd.clear();
 	
 	retrieveSQL(entries);
 	entries.erase(0);
@@ -14,6 +15,11 @@ int main(int argc, char* argv[]){
 	string str = entries.find(10993472)->second.getFirstName().c_str();
 	
 	//sprintf(strbuf, "%s", buf);
+	lcd.move(5,0);
+	printfl(str, lcd);
+	
+	string str = entries.find(10993472)->second.getLastName().c_str();
+	lcd.move(5,1);
 	printfl(str, lcd);
 
 	return 0;
