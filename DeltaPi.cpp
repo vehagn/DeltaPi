4a196582c07c9eacb079b5aa7c9311f4b08eba8e
vehagn <veghag@gmail.com>
2013-07-28 03:11:44 +0200

    Update DeltaPi.cpp

--- DeltaPi.cpp
+++ DeltaPi.cpp
@@ -12,13 +12,14 @@ int main(int argc, char* argv[]){
 	retrieveSQL(entries);
 	entries.erase(0);
 	
-	string str = entries.find(10993472)->second.getFirstName().c_str();
+	string str;
+	str = entries.find(10993472)->second.getFirstName().c_str();
 	
 	//sprintf(strbuf, "%s", buf);
 	lcd.move(5,0);
 	printfl(str, lcd); printf("\n");
 	
-	string str = entries.find(10993472)->second.getLastName().c_str();
+	str = entries.find(10993472)->second.getLastName().c_str();
 	lcd.move(5,1);
 	printfl(str, lcd); printf("\n");
 
