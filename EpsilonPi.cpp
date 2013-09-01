9f8a73d30aa40d56db6a20eac3cec53975c071c0
vehagn <veghag@gmail.com>
2013-09-01 21:57:52 +0200

    test

--- EpsilonPi.cpp
+++ EpsilonPi.cpp
@@ -3,18 +3,36 @@
 int main(int argc, char* argv[]){
 	
 	gpio io;
+	//Office
 	io.setup(11, gpio::INPUT);
 	io.setup(4, gpio::OUTPUT);
-	bool value = false;
+	
+	//Coffee
+	io.setup(10, gpio::INPUT);
+	io.setup(22, gpio::OUTPUT);
+	
+	bool office = false;
+	bool coffee = false;
+	time_t coffeeTime = 0;
 	
 	while (true){
-		//value = io.read(11);
-		io.write(4, gpio::HIGH);
-		time::sleep(1);
-		io.write(4, gpio::LOW);
-		time::sleep(1);
-		printf("GPIO 11: %s\n",(value)?("HIGH"):("LOW"));	
-	}
-	
+		office = io.read(11);
+		coffee = io.read(10);
+		
+		if office{
+			io.write(4, gpio::HIGH);
+		}else
+			io.write(4, gpio::LOW);
+		}
+		
+		if coffe{
+			time(&coffeTime);
+		}
+		if difftime(time(NULL),coffeTime) < 1*10{
+			io.write(22, gpio::HIGH);
+		}else{
+			io.write(22, gpio::LOW);
+		}		
+	}	
 	return 0;
 }
