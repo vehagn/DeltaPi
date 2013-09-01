6582621ddcc401f0d314000bdeb34d00cb1d1d4c
vehagn <veghag@gmail.com>
2013-09-01 19:20:03 +0200

    Output test

--- EpsilonPi.cpp
+++ EpsilonPi.cpp
@@ -4,12 +4,15 @@ int main(int argc, char* argv[]){
 	
 	gpio io;
 	io.setup(11, gpio::INPUT);
+	io.setup(4, gpio::OUTPUT);
 	bool value = false;
 	
 	while (true){
-		value = io.read(11);
+		//value = io.read(11);
+		io.write(4, gpio::HIGH);
 		time::sleep(1);
-		
+		io.write(4, gpio::LOW);
+		time::sleep(1);
 		printf("GPIO 11: %s\n",(value)?("HIGH"):("LOW"));	
 	}
 	
