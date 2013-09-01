1a4b470c219c836e9bd308f44e5fcc83f1797420
vehagn <veghag@gmail.com>
2013-09-02 00:51:12 +0200

    .

--- EpsilonPi.cpp
+++ EpsilonPi.cpp
@@ -53,7 +53,7 @@ int main(int argc, char* argv[]){
 		}	
 		if (difftime(time(NULL),coffeeTime) <= 1*10){
 			io.write(22, rpihw::gpio::HIGH);
-			if ((coffee_prev != coffee) && (difftime(coffeePress,coffeeTime) > 2)){
+			if ((coffee_prev != coffee) && (difftime(coffeeTime,coffeePress) > 2)){
 				time(&coffeePress);
 				coffee_prev = coffee;
 				timeinfo1 = localtime(&coffeeTime);
