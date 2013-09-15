56c0a326807c6281431b0ed018a99a4897586123
vehagn <veghag@gmail.com>
2013-09-15 20:22:54 +0200

    .

--- functions.cpp
+++ functions.cpp
@@ -218,7 +218,7 @@ void printSummary(map<const int, Entry> &entries, hd44780 &lcd){
 			money += i->second.getCash();
 		}
 		spent += i->second.getSpent();
-		printf("%li\t%li\t%s\n",spent, i->second.getSpent(),i->second.getFirstName());
+		printf("%li\t%li\t%s\n",spent, i->second.getSpent(),i->second.getFirstName().c_str());
 	}
 	lcd.clear();
 	sprintf(buf, "Persons:    %5i", persons);
