5ffb7f79f3c46dc18960e40ccc24a00d30e35d43
vehagn <veghag@gmail.com>
2013-09-15 22:43:42 +0200

    .

--- functions.cpp
+++ functions.cpp
@@ -257,9 +257,7 @@ void printLastCoffee(hd44780 &lcd){
 	printfl("Last coffee:", lcd);
 	lcd.move(0,2);
 	coffeeFile = fopen("/var/www/pi.deltahouse.no/public_html/coffee.txt","r");
-	while ((buf[(i++)%128] = getc(coffeeFile)) != EOF){
-		printf("%c",buf[i]);
-	}
+	fread(buf,1,128,coffeeFile);
 	fclose(coffeeFile);
 	printfl(buf, lcd);
 	lcd.setAutoscroll(hd44780::HSCROLL_LINE | hd44780::VSCROLL);
