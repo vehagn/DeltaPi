d774b42c3257334496695e668f6a75a5b7d35d12
vehagn <veghag@gmail.com>
2015-08-19 01:44:53 +0200

    fix

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 sleep 1
-echo Starting DeltaPi
-/usr/local/DeltaPi
-sleep 1
 echo Starting EpsilonPi
 /usr/local/EpsilonPi
+sleep 1
+echo Starting DeltaPi
+/usr/local/DeltaPi
 echo Shutting down in 10 s. ctrl-c to cancel shutdown.
 sleep 10
 shutdown -h now
