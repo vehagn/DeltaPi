8ca22f08754016884a4fa6f374e1e229e254c441
vehagn <veghag@gmail.com>
2015-08-19 01:32:11 +0200

    .

--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-sleep 5
+sleep 1
+echo Starting DeltaPi
 /usr/local/DeltaPi
-sleep 2
+sleep 1
+echo Starting EpsilonPi
 /usr/local/EpsilonPi
+echo Shutting down in 10 s. ctrl-c to cancel shutdown.
+sleep 10
 shutdown -h now
