5ea8da74fe61c98cbb842901e77d7c07dffa4ed5
vehagn <veghag@gmail.com>
2015-08-19 00:56:38 +0200

    .

--- header.h
+++ header.h
@@ -39,7 +39,6 @@ using namespace sql;
 using namespace rpihw::iface;
 using namespace rpihw::display;
 using namespace rpihw::utils;
-using namespace rpihw::gpio;
 
 extern string DBHOST, USER, PASSWORD, DATABASE;
 
